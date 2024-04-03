3231db68afc58b811ab310a9097fe0e1a8a1df02
Jó Sales <143146309+Josales9@users.noreply.github.com>
2024-04-03 17:38:31 -0300

    Update Contato.jsx

--- src/routes/Contato.jsx
+++ src/routes/Contato.jsx
@@ -7,7 +7,27 @@ import Facebook from '../assets/img/facebook 1.svg'
 function Contato() {
   return (
     <>
-
+    <div className="contact-container">
+      <div className="contact-content">
+        <div className="contact-info">
+          <p>SportPlace@esportplace.com</p>
+          <p>Avenida Leon Jurn, 5028<br />Brasil, São Paulo, SP</p>
+          <div className="social-links">
+            <a href=""><img src={Whatsapp} alt="" /></a>
+            <a href=""><img src={Instagram} alt="" /></a>
+            <a href=""><img src={Facebook} alt="" /></a>
+          </div>
+        </div>
+        <div className="forma-container">
+          <form className="contact-forma">
+            <input type="text" placeholder="Nome" />
+            <input type="email" placeholder="Email" />
+            <textarea placeholder="Como podemos ajudar?" />
+            <button type="submit">Enviar</button>
+          </form>
+        </div>
+      </div>
+    </div>
     </>
   );
 }
