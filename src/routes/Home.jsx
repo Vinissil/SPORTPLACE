4f2f6a97ad4aed0f079c13d2297f12f94d6bc08c
Gabriel Mediotti <49275907+mediotti@users.noreply.github.com>
2024-04-03 21:46:53 +0000

    feat: Add homepage

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -4,7 +4,21 @@ import '../css/estilo.css';
 function Home() {
   return (
     <>
-
+          <div className="home-container">
+      <div className="slider">
+        <div className="slide active">
+          <div className="runner-image"></div>
+        </div>
+      </div>
+      <div className="subscription-box">
+        <p>RECEBA NOSSAS PROMOÇÕES</p>
+        <input type="text" placeholder="Digite seu Email" />
+        <button>Receber</button>
+      </div>
+      <div className="brands">
+        <div className="brand nike"></div>
+      </div>
+    </div>
     </>
   );
 }
