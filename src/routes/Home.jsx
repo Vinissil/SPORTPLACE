667c3d47e272219dfcd328e519cab484e7b2d3b7
Vini <136639194+Vinissil@users.noreply.github.com>
2024-04-03 17:33:42 -0300

    Update Home.jsx

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -4,21 +4,7 @@ import '../css/estilo.css';
 function Home() {
   return (
     <>
-          <div className="home-container">
-      <div className="slider">
-        <div className="slide active">
-          <div className="runner-image"></div>
-        </div>
-      </div>
-      <div className="subscription-box">
-        <p>RECEBA NOSSAS PROMOÇÕES</p>
-        <input type="text" placeholder="Digite seu Email" />
-        <button>Receber</button>
-      </div>
-      <div className="brands">
-        <div className="brand nike"></div>
-      </div>
-    </div>
+
     </>
   );
 }
