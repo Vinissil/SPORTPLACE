c084de1c7cab08d52c3c9fe71940d869ca1652b2
Jó Sales <143146309+Josales9@users.noreply.github.com>
2024-04-03 17:28:51 -0300

    Update imports contact.jsx

--- src/routes/Contato.jsx
+++ src/routes/Contato.jsx
@@ -1,11 +1,5 @@
 import {} from 'react';
 import '../css/estilo.css';
-
-function Contato() {
-  return (
-    <>
-    
-    </>
-  );
-}
-export default Contato;
+import Whatsapp from '../assets/img/whatsapp 1.svg'
+import Instagram from '../assets/img/instagram 1.svg'
+import Facebook from '../assets/img/facebook 1.svg'
