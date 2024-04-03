081d1861197b5b0896d0e3690bb15ba245a8d81c
Vini <136639194+Vinissil@users.noreply.github.com>
2024-04-03 17:00:12 -0300

    comitando pagina  produto.jsx

--- src/routes/produto.jsx
+++ src/routes/produto.jsx
@@ -4,9 +4,49 @@ import '../css/estilo.css';
 const Produto = () => {
   return (
     <>
-    
+    <div className="product-page">
+      <div className="category sports">
+        <div className="category-title"><a href="produtos.jsx">Esportes</a></div>
+      </div>
+      <div className="category feminine">
+        <div className="category-title"><a href="produtos.jsx">Feminina</a></div>
+      </div>
+      <div className="category masculine">
+        <div className="category-title"><a href="produtos.jsx">Masculino</a></div>
+      </div>
+      <div className="product-list">
+      <div className="product card">
+        <div className="product-image blue-shoe"></div>
+        <div className="product-info">
+          <p>Nome</p>
+          <p>R$ 99,99</p>
+        </div>
+      </div>
+      <div className="product card">
+        <div className="product-image green-shoe"></div>
+        <div className="product-info">
+          <p>Nome</p>
+          <p>R$ 99,99</p>
+        </div>
+      </div>
+      <div className="product card">
+        <div className="product-image orange-shoe"></div>
+        <div className="product-info">
+          <p>Nome</p>
+          <p>R$ 99,99</p>
+        </div>
+      </div>
+      <div className="product card">
+        <div className="product-image green-shoe"></div>
+        <div className="product-info">
+          <p>Nome</p>
+          <p>R$ 99,99</p>
+        </div>
+      </div>
+    </div>
+      </div>
     </>
-    );
+  );
 };
 
 export default Produto;
